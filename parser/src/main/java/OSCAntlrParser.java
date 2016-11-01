ace0beba5868f72365845a6f11e2813ffc8fe28e
Kyle Bolton <latestthing@gmail.com>
2016-11-01 09:33:48 -0500

    fleshed out structure and added lots of TODOs

--- parser/src/main/java/OSCAntlrParser.java
+++ parser/src/main/java/OSCAntlrParser.java
@@ -4,29 +4,45 @@
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Map;
 
 import edu.uh.cs.os_c.*;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.misc.IntegerList;
 import org.antlr.v4.runtime.tree.ParseTree;
 //import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 public class OSCAntlrParser{
-    void parseAndRun(FileInputStream source_code_input_stream) throws IOException {
+    private OS_CParser parser;
+    private ParseTree tree;
+    private FileInputStream source_code_input_stream;
+    private FileInputStream data_input_stream;
+
+    public ParseTree parse(FileInputStream source_code_input_stream, FileInputStream data_input_stream) throws IOException {
+        this.source_code_input_stream = source_code_input_stream;
+        this.data_input_stream = data_input_stream;
         ANTLRInputStream input = new ANTLRInputStream(source_code_input_stream);
         OS_CLexer lexer = new OS_CLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        OS_CParser parser = new OS_CParser(tokens);
-        ParseTree tree = parser.compilationUnit(); // see the grammar ->
-        // starting point for
-        // parsing a java file
-
-
-
-        OS_CVisitor visitor = new OS_CVisitorIo(); // extends JavaBaseVisitor<Void>
-        // and overrides the methods
-        // you're interested
-        visitor.visit(tree);
+        parser = new OS_CParser(tokens);
+        tree = parser.compilationUnit();
+    }
+    public Map<String, Integer> getInputVariables() throws IOException {
+        ArrayList<String> input_variables;
+        OS_CVisitorRead visitor = new OS_CVisitorRead(); // extends JavaBaseVisitor<Void>
+        input_variables = visitor.visit(tree);
+        //TODO loop through variables and read them from data file
+        return input_variables;
+    }
+    public void runCalculations(Map<String, Integer> variables) {
+        //TODO implement OS_CVisitorCalculations
+    }
+    public void write(Map<String, Integer> variables, PrintStream out){
+        //TODO implement OS_CVisitorWrite
+        //TODO write all specified variables to out
     }
 }
--- parser/src/main/java/Runner.java
+++ parser/src/main/java/Runner.java
@@ -4,15 +4,28 @@
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Map;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 public class Runner {
     public static void main(String args[]) {
         String code_filepath = args[0]; //TODO error checking?
+        Boolean verbose; //TODO get from args
         try (FileInputStream source_code_input_stream = new FileInputStream(code_filepath)) {
             OSCAntlrParser parser = new OSCAntlrParser(source_code_input_stream);
-            parser.parseAndRun(source_code_input_stream);//TODO decide which one gets the input stream
+            ParseTree tree = parser.parse(source_code_input_stream);//TODO decide which one gets the input stream
+            if(verbose){
+                //TODO print tree
+            }
+            Map<String, Integer> variables;
+            parser.getInputVariables();
+            parser.runCalculations(variables);
+            parser.write(variables, System.out);
+
         } catch (IOException e) {
-            System.err.printf("failed to load config file: %s\n", configFileName);
+            System.err.printf("failed to load config file: %s\n", code_filepath);
         }
     }
 }
